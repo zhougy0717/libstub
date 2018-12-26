a71a1f4cf1d32efaccb55e7bd8159d8a4e5e910e
Ben Zhou <zhougy0717@gmail.com>
2018-12-26 20:59:27 +0800

    Support verify pointer args.

--- stub.h
+++ stub.h
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdarg.h>
+#include <assert.h>
 
 struct stub_obj {
     bool called;
@@ -12,69 +13,128 @@ struct stub_obj {
     void (*side_effect)(void);
 };
 
-#define FORMAL_ARGS1(a) \
-    a arg1
+#define MAX_CALL_TIMES 5
+#define LAST_CALL_ARGS(name) \
+    name##_stub_obj.args[name##_stub_obj.common_data.call_times % MAX_CALL_TIMES]
+
+#define FORMAL_ARGS0()
+#define FORMAL_ARGS1(a) a arg1
 #define FORMAL_ARGS2(a, b) \
     FORMAL_ARGS1(a), b arg2
+#define FORMAL_ARGS3(a, b, c) \
+    FORMAL_ARGS2(a, b), c arg3
+#define FORMAL_ARGS4(a, b, c, d) \
+    FORMAL_ARGS3(a, b, c), d arg4
+#define FORMAL_ARGS5(a, b, c, d, e) \
+    FORMAL_ARGS4(a, b, c, d), e arg5
+#define FORMAL_ARGS6(a, b, c, d, e, f) \
+    FORMAL_ARGS5(a, b, c, d, e), f arg6
 #define FORMAL_ARGS(n, ...) FORMAL_ARGS##n(__VA_ARGS__)
 
+#define ACTUAL_ARGS0
 #define ACTUAL_ARGS1 arg1
-#define ACTUAL_ARGS2 arg1, arg2
+#define ACTUAL_ARGS2 ACTUAL_ARGS1, arg2
+#define ACTUAL_ARGS3 ACTUAL_ARGS2, arg3
+#define ACTUAL_ARGS4 ACTUAL_ARGS3, arg4
+#define ACTUAL_ARGS5 ACTUAL_ARGS4, arg5
+#define ACTUAL_ARGS6 ACTUAL_ARGS5, arg6
 
-#define STRUCT_ARGS1(a) a arg1
+#define STRUCT_ARGS0() int place_holder;
+#define STRUCT_ARGS1(a) a arg1;
 #define STRUCT_ARGS2(a, b) \
     STRUCT_ARGS1(a); \
     b arg2;
+#define STRUCT_ARGS3(a, b, c) \
+    STRUCT_ARGS2(a, b); \
+    c arg3;
+#define STRUCT_ARGS4(a, b, c, d) \
+    STRUCT_ARGS3(a, b, c); \
+    d arg4;
+#define STRUCT_ARGS5(a, b, c, d, e) \
+    STRUCT_ARGS4(a, b, c, d); \
+    e arg5;
+#define STRUCT_ARGS6(a, b, c, d, e, f) \
+    STRUCT_ARGS5(a, b, c, d, e); \
+    f arg6;
 #define STRUCT_ARGS(arg_nr, ...) \
     STRUCT_ARGS##arg_nr(__VA_ARGS__)
 
+#define SAVE_ARG0(p)
 #define SAVE_ARG1(p) p.arg1 = arg1;
 #define SAVE_ARG2(p) \
     p.arg2 = arg2; \
     SAVE_ARG1(p)
+#define SAVE_ARG3(p) \
+    p.arg3 = arg3; \
+    SAVE_ARG2(p)
+#define SAVE_ARG4(p) \
+    p.arg4 = arg4; \
+    SAVE_ARG3(p)
+#define SAVE_ARG5(p) \
+    p.arg5 = arg5; \
+    SAVE_ARG4(p)
+#define SAVE_ARG6(p) \
+    p.arg6 = arg6; \
+    SAVE_ARG5(p)
 #define SAVE_ARG(p, n) SAVE_ARG##n(p)
 
+#define VERIFY_ARG0(p) true
 #define VERIFY_ARG1(p) (p.arg1 == arg1)
 #define VERIFY_ARG2(p) \
     (p.arg2 == arg2) || \
     VERIFY_ARG1(p)
+#define VERIFY_ARG3(p) \
+    (p.arg3 == arg3) || \
+    VERIFY_ARG2(p)
+#define VERIFY_ARG4(p) \
+    (p.arg4 == arg4) || \
+    VERIFY_ARG3(p)
+#define VERIFY_ARG5(p) \
+    (p.arg5 == arg5) || \
+    VERIFY_ARG4(p)
+#define VERIFY_ARG6(p) \
+    (p.arg6 == arg6) || \
+    VERIFY_ARG5(p)
 #define VERIFY_ARGS(p, n) VERIFY_ARG##n(p)
 
-#define DEFINE_FUNCTION_STUB(name, return_type, arg_nr, ...) \
+#define DEFINE_FUNCTION_STUB_CORE(name, return_type, arg_nr, ...) \
     struct name##_args { \
         STRUCT_ARGS(arg_nr, __VA_ARGS__)\
     };\
     struct name##_stub_obj { \
         struct stub_obj common_data; \
-        struct __args { \
+        struct { \
             STRUCT_ARGS(arg_nr, __VA_ARGS__); \
-        } args;\
+        } args[MAX_CALL_TIMES];\
     } name##_stub_obj; \
-    static void name##_record(int args, FORMAL_ARGS(arg_nr, __VA_ARGS__))\
+    static void name##_record(FORMAL_ARGS(arg_nr, __VA_ARGS__))\
     { \
         struct stub_obj* pobj = (struct stub_obj*)(&name##_stub_obj); \
         pobj->called = true; \
-        SAVE_ARG(name##_stub_obj.args, arg_nr); \
+        SAVE_ARG(LAST_CALL_ARGS(name), arg_nr); \
     }\
     static void name##_restore(void) \
     { \
         struct stub_obj* pobj = (struct stub_obj*)(&name##_stub_obj); \
         memset(pobj, 0, sizeof(name##_stub_obj)); \
     } \
+    static bool name##_verify_args(FORMAL_ARGS(arg_nr, __VA_ARGS__)) \
+    {\
+        return VERIFY_ARGS(LAST_CALL_ARGS(name), arg_nr); \
+    }
+
+#define DEFINE_FUNCTION_STUB(name, return_type, arg_nr, ...) \
+    DEFINE_FUNCTION_STUB_CORE(name, return_type, arg_nr, __VA_ARGS__)\
     static void name##_returns(return_type value) \
     {\
         struct stub_obj* pobj = (struct stub_obj*)(&name##_stub_obj); \
         memcpy(pobj->return_buffer, &value, sizeof(value)); \
     }\
-    static bool name##_verify_args(FORMAL_ARGS(arg_nr, __VA_ARGS__)) \
-    {\
-        return VERIFY_ARGS(name##_stub_obj.args, arg_nr); \
-    }\
     return_type name(FORMAL_ARGS(arg_nr, __VA_ARGS__))\
     {\
         struct stub_obj* pobj = (struct stub_obj*)(&name##_stub_obj); \
-        name##_record(arg_nr, ACTUAL_ARGS##arg_nr); \
         pobj->call_times ++; \
+        name##_record(ACTUAL_ARGS##arg_nr); \
         return_type result = *(return_type*)(pobj->return_buffer); \
         if (pobj->side_effect != NULL) \
         { \
@@ -82,17 +142,34 @@ struct stub_obj {
         } \
         return result; \
     }
-#define STUB(name) ((struct stub_obj*)&name##_stub_obj)
 
-#define FETCH_ARG_OF(name, idx, stub_obj) \
-    name##_stub_obj.args.arg##idx
+#define DEFINE_FUNCTION_STUB_NO_RETURN(name, return_type, arg_nr, ...) \
+    DEFINE_FUNCTION_STUB_CORE(name, return_type, arg_nr, __VA_ARGS__)\
+    return_type name(FORMAL_ARGS(arg_nr, __VA_ARGS__))\
+    {\
+        struct stub_obj* pobj = (struct stub_obj*)(&name##_stub_obj); \
+        pobj->call_times ++; \
+        name##_record(ACTUAL_ARGS##arg_nr); \
+        if (pobj->side_effect != NULL) \
+        { \
+            pobj->side_effect(); \
+        } \
+    }
 
-#define SET_RETURN_OF(name, value) \
-    name##_returns(value);
+#define FETCH_ARG_OF(name, idx) LAST_CALL_ARGS(name).arg##idx
+
+#define SET_RETURN_OF(name, value) name##_returns(value);
 
 #define RESTORE(name) name##_restore()
 #define CALLED(name) name##_stub_obj.common_data.called
 #define CALL_TIMES(name) name##_stub_obj.common_data.call_times
 #define SET_SIDE_EFFECT_OF(name, func) name##_stub_obj.common_data.side_effect = func
 #define CALL_WITH_ARGS(name, ...) name##_verify_args(__VA_ARGS__)
+#define FETCH_ARG_FROM_CALL(name, call, idx) \
+    { \
+        assert(idx <= MAX_CALL_TIMES); \
+        name##_stub_obj.args[call % MAX_CALL_TIMES].arg##idx; \
+    }
+#define VERIFY_POINTER_ARG(compare, name, idx) \
+    (memcmp(compare, FETCH_ARG_OF(name, idx), sizeof(*(compare))))
 #endif
--- tests/main.c
+++ tests/main.c
@@ -11,13 +11,11 @@ int sub(int a, int b)
 DEFINE_FUNCTION_STUB(func, int, 2, int, char);
 
 START_TEST(test_stub_called) {
-    struct stub_obj* func_stub = STUB(func);
     func(0, 0);
     ck_assert(CALLED(func));
 }
 END_TEST
 START_TEST(test_stub_restore) {
-    struct stub_obj* func_stub = STUB(func);
     func(0, 0);
     RESTORE(func);
     ck_assert(!CALLED(func));
@@ -27,14 +25,12 @@ START_TEST(test_stub_restore) {
 }
 END_TEST
 START_TEST(test_extract_args) {
-    struct stub_obj* func_stub = STUB(func);
     func(1, 3);
-    ck_assert_int_eq(1, FETCH_ARG_OF(func, 1, func_stub));
-    ck_assert_int_eq(3, FETCH_ARG_OF(func, 2, func_stub));
+    ck_assert_int_eq(1, FETCH_ARG_OF(func, 1));
+    ck_assert_int_eq(3, FETCH_ARG_OF(func, 2));
 }
 END_TEST
 START_TEST(test_returns) {
-    struct stub_obj* func_stub = STUB(func);
     SET_RETURN_OF(func, 123);
     ck_assert_int_eq(123, func(0,1));
     ck_assert_int_eq(123, func(2,3));
@@ -42,7 +38,6 @@ START_TEST(test_returns) {
 END_TEST
 
 START_TEST(test_call_times) {
-    struct stub_obj* func_stub = STUB(func);
     func(1,2);
     ck_assert_int_eq(1, CALL_TIMES(func));
 
@@ -69,6 +64,60 @@ START_TEST(test_call_with_args) {
     // ck_assert(CALL_WITH_ARGS(20)); <----- compile error
 }
 END_TEST
+START_TEST(test_fetch_arg_of_call_time) {
+    func(10, 20);
+    func(40, 30);
+    ck_assert_int_eq(40, FETCH_ARG_FROM_CALL(func, 2, 1));
+    // ck_assert_int_eq(40, FETCH_ARG_FROM_CALL(func, MAX_CALL_TIMES, 1)); <----- You will be asserted here.
+}
+END_TEST
+
+// libstub only supports to stub a function with up to 6 args.
+DEFINE_FUNCTION_STUB(long_arglist_func, int, 6, int, char, int, int, int, int);
+
+START_TEST(test_long_arg_list) {
+    long_arglist_func(0,0,0,0,0,0);
+    ck_assert(CALLED(long_arglist_func));
+}
+END_TEST
+
+DEFINE_FUNCTION_STUB(no_arg_func, int, 0)
+START_TEST(test_no_arg) {
+    no_arg_func();
+    ck_assert(CALLED(no_arg_func));
+}
+END_TEST
+
+DEFINE_FUNCTION_STUB_NO_RETURN(void_func, void, 1, int);
+START_TEST(test_void_func) {
+    void_func(5);
+    ck_assert(CALLED(void_func));
+    ck_assert_int_eq(5, FETCH_ARG_OF(void_func, 1));
+    ck_assert_int_eq(5, FETCH_ARG_FROM_CALL(void_func, 1, 1));
+}
+END_TEST
+
+struct args{
+    int a;
+    int b;
+};
+DEFINE_FUNCTION_STUB(func_with_pointer_arg, int, 1, struct args*);
+START_TEST(test_verify_pointer_arg) {
+    struct args c = {12345, 6789};
+    struct args d = {12345, 6789};
+    func_with_pointer_arg(&c);
+    ck_assert_int_eq(0, VERIFY_POINTER_ARG(&d, func_with_pointer_arg, 1));
+}
+END_TEST
+
+DEFINE_FUNCTION_STUB(func_with_void_pointer_arg, int, 1, void*);
+START_TEST(test_verify_void_pointer_arg) {
+    struct args c = {12345, 6789};
+    struct args d = {12345, 6789};
+    func_with_void_pointer_arg(&c);
+    ck_assert_int_eq(0, VERIFY_POINTER_ARG(&d, func_with_void_pointer_arg, 1));
+}
+END_TEST
 Suite * make_sub_suite(void) {
     Suite *s = suite_create("stub testing");       // 建立Suite
     TCase *tc_sub = tcase_create("test function stubs");  // 建立测试用例集
@@ -79,6 +128,12 @@ Suite * make_sub_suite(void) {
     tcase_add_test(tc_sub, test_call_times);
     tcase_add_test(tc_sub, test_set_side_effect);
     tcase_add_test(tc_sub, test_call_with_args);
+    tcase_add_test(tc_sub, test_fetch_arg_of_call_time);
+    tcase_add_test(tc_sub, test_long_arg_list);
+    tcase_add_test(tc_sub, test_no_arg);
+    tcase_add_test(tc_sub, test_void_func);
+    tcase_add_test(tc_sub, test_verify_pointer_arg);
+    tcase_add_test(tc_sub, test_verify_void_pointer_arg);
 
     return s;
 }
